4f0ed04135893200e80914328f1bc7275db9b137
IE-MangChi <hsm983278@gmail.com>
2023-11-13 22:33:15 +0900

    feat: 특별 할인 이벤트 기능 구현
    - support: 특별 할인 이벤트 가능 여부 판단
    - discount: 할인 기능

--- src/main/java/christmas/domain/event/EventConfig.java
+++ src/main/java/christmas/domain/event/EventConfig.java
@@ -2,10 +2,11 @@ package christmas.domain.event;
 
 public class EventConfig {
 
-    public static final int INITAL_DISCOUNT_AMOUNT = 1000;
+    public static final int INITAL_DISCOUNT_AMOUNT = 1_000;
     public static final int DAILY_DISCOUNT_INCREASE = 100;
     public static final int START_DATE = 1;
     public static final int END_DATE = 25;
-
     public static final int MINIMUM_ORDER_AMOUNT_FOR_GIFT_EVENT = 120_000;
+    public static final int[] SPECIAL_DISCOUNT_DAY = {3, 10, 17, 24, 25, 31};
+    public static final Integer SPECIAL_DISCOUNT_AMOUNT = 1_000;
 }
---
+++ src/main/java/christmas/domain/event/SpecialEvent.java
@@ -0,0 +1,27 @@
+package christmas.domain.event;
+
+import java.util.Arrays;
+
+public enum SpecialEvent implements Event<Integer, Integer>{
+
+    STAR(EventConfig.SPECIAL_DISCOUNT_DAY);
+
+    private int[] dayWithStar;
+
+    SpecialEvent(int... dayWithStar) {
+        this.dayWithStar = dayWithStar;
+    }
+
+
+
+    @Override
+    public boolean support(Integer day) {
+        return Arrays.stream(dayWithStar)
+                .anyMatch(dayWithStar -> dayWithStar == day);
+    }
+
+    @Override
+    public Integer discount(Integer value) {
+        return EventConfig.SPECIAL_DISCOUNT_AMOUNT;
+    }
+}
