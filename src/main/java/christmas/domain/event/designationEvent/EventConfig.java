b88f8ed331565c330628612efe9d6d34d57bcd51
IE-MangChi <hsm983278@gmail.com>
2023-11-14 21:09:14 +0900

    fix: OrderAmount 클래스에서 사용해야하므로 접근제어자 public으로 변경

--- src/main/java/christmas/domain/event/designationEvent/EventConfig.java
+++ src/main/java/christmas/domain/event/designationEvent/EventConfig.java
@@ -3,7 +3,7 @@ package christmas.domain.event.designationEvent;
 public class EventConfig {
 
     // 이벤트 배지
-    protected static final long STAR_BADGE_MINIMUM_AMOUNT = 5_000;
-    protected static final long TREE_BADGE_MINIMUM_AMOUNT = 10_000;
-    protected static final long SANTA_BADGE_MINIMUM_AMOUNT = 20_000;
+    public static final long STAR_BADGE_MINIMUM_AMOUNT = 5_000;
+    public static final long TREE_BADGE_MINIMUM_AMOUNT = 10_000;
+    public static final long SANTA_BADGE_MINIMUM_AMOUNT = 20_000;
 }
--- src/main/java/christmas/domain/event/discountEvent/EventConfig.java
+++ src/main/java/christmas/domain/event/discountEvent/EventConfig.java
@@ -6,29 +6,29 @@ import java.util.List;
 
 public class EventConfig {
 
-    protected static final int YEAR = 2023;
-    protected static final int MONTH = 12;
+    public static final int YEAR = 2023;
+    public static final int MONTH = 12;
 
     // 디데이 할인
-    protected static final Long INITAL_DISCOUNT_AMOUNT = 1_000L;
-    protected static final Long DAILY_DISCOUNT_INCREASE = 100L;
-    protected static final int START_DATE = 1;
-    protected static final int END_DATE = 25;
+    public static final Long INITAL_DISCOUNT_AMOUNT = 1_000L;
+    public static final Long DAILY_DISCOUNT_INCREASE = 100L;
+    public static final int START_DATE = 1;
+    public static final int END_DATE = 25;
 
     // 증정 이벤트
-    protected static final Long MINIMUM_ORDER_AMOUNT_FOR_GIFT_EVENT = 120_000L;
+    public static final Long MINIMUM_ORDER_AMOUNT_FOR_GIFT_EVENT = 120_000L;
 
     // 특별 할인
-    protected static final int[] SPECIAL_DISCOUNT_DAY = {3, 10, 17, 24, 25, 31};
-    protected static final Long SPECIAL_DISCOUNT_AMOUNT = 1_000L;
+    public static final int[] SPECIAL_DISCOUNT_DAY = {3, 10, 17, 24, 25, 31};
+    public static final Long SPECIAL_DISCOUNT_AMOUNT = 1_000L;
 
     // 평일 할인
-    protected static final List<DayOfWeek> WEEKDAY_DISCOUNT_DAY_OF_WEEK = List.of(DayOfWeek.SUNDAY, DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY);
-    protected static final List<MenuCategory> WEEKDAY_DISCOUNT_MENU = List.of(MenuCategory.CHOCO_CAKE, MenuCategory.ICE_CREAM);
-    protected static final long WEEKDAY_DISCOUNT_AMOUNT = 2_023;
+    public static final List<DayOfWeek> WEEKDAY_DISCOUNT_DAY_OF_WEEK = List.of(DayOfWeek.SUNDAY, DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY);
+    public static final List<MenuCategory> WEEKDAY_DISCOUNT_MENU = List.of(MenuCategory.CHOCO_CAKE, MenuCategory.ICE_CREAM);
+    public static final long WEEKDAY_DISCOUNT_AMOUNT = 2_023;
 
     // 주말 할인
-    protected static final List<DayOfWeek> WEEKEND_DISCOUNT_DAY_OF_WEEK = List.of(DayOfWeek.FRIDAY, DayOfWeek.SATURDAY);
-    protected static final List<MenuCategory> WEEKEND_DISCOUNT_MENU = List.of(MenuCategory.T_BONE_STEAK, MenuCategory.BBQ_RIB, MenuCategory.CHRISTMAS_PASTA, MenuCategory.SEAFOOD_PASTA);
-    protected static final long WEEKEND_DISCOUNT_AMOUNT = 2_023;
+    public static final List<DayOfWeek> WEEKEND_DISCOUNT_DAY_OF_WEEK = List.of(DayOfWeek.FRIDAY, DayOfWeek.SATURDAY);
+    public static final List<MenuCategory> WEEKEND_DISCOUNT_MENU = List.of(MenuCategory.T_BONE_STEAK, MenuCategory.BBQ_RIB, MenuCategory.CHRISTMAS_PASTA, MenuCategory.SEAFOOD_PASTA);
+    public static final long WEEKEND_DISCOUNT_AMOUNT = 2_023;
 }
