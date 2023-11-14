0c6a007eb4f33bb087afd883ba4e890b6b665bf9
IE-MangChi <hsm983278@gmail.com>
2023-11-14 18:28:11 +0900

    fix: 혜택 금액 5천원만 넘으면 별 배지만 지급하던 오류 해결, NOTHING enum추가

--- src/main/java/christmas/domain/event/designationEvent/BadgeEvent.java
+++ src/main/java/christmas/domain/event/designationEvent/BadgeEvent.java
@@ -2,6 +2,7 @@ package christmas.domain.event.designationEvent;
 
 public enum BadgeEvent {
 
+    NOTHING("없음"),
     STAR("별"),
     TREE("트리"),
     SANTA("산타");
@@ -13,16 +14,16 @@ public enum BadgeEvent {
     }
 
     public static BadgeEvent getBadgeByTotalBenefitAmount(long amount) {
-        if (amount >= EventConfig.STAR_BADGE_MINIMUM_AMOUNT) {
-            return STAR;
+        if (amount >= EventConfig.SANTA_BADGE_MINIMUM_AMOUNT) {
+            return SANTA;
         }
         if (amount >= EventConfig.TREE_BADGE_MINIMUM_AMOUNT) {
             return TREE;
         }
-        if (amount >= EventConfig.SANTA_BADGE_MINIMUM_AMOUNT) {
-            return SANTA;
+        if (amount >= EventConfig.STAR_BADGE_MINIMUM_AMOUNT) {
+            return STAR;
         }
-        return null;
+        return NOTHING;
     }
 
     public String getBadge() {
