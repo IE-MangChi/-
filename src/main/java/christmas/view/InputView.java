1454f872d013236e01d3d4bf0659dfff7ab02be8
IE-MangChi <hsm983278@gmail.com>
2023-11-14 19:54:55 +0900

    refactor: 에러메시지 분리 fix: 날짜 정규표현식 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,6 +1,8 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.exception.ChristmasException;
+import christmas.exception.ErrorMessage;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -8,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    private Pattern datePattern = Pattern.compile("^(?:[1-9]|[1-2][0-9]|3[0-1])$");
+    private final Pattern datePattern = Pattern.compile("^[1-9]|[1-2][0-9]|[3][0-1]$");
 
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
@@ -21,21 +23,16 @@ public class InputView {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
         try {
-            return Arrays.stream(input.split(","))
-                    .map(String::trim)
-                    .map(order -> order.split("-"))
-                    .collect(Collectors.toMap(
-                            order -> order[0],
-                            order -> Integer.parseInt(order[1])
-                    ));
+            return Arrays.stream(input.split(",")).map(String::trim).map(order -> order.split("-"))
+                    .collect(Collectors.toMap(order -> order[0], order -> Integer.parseInt(order[1])));
         } catch (IllegalStateException exception) {
-            throw new IllegalStateException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.");
+            throw ChristmasException.of(ErrorMessage.INVALID_ORDER);
         }
     }
 
     private void validateDate(String input) {
         if (!datePattern.matcher(input).matches()) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            throw ChristmasException.of(ErrorMessage.INVALID_DATE);
         }
     }
 }
