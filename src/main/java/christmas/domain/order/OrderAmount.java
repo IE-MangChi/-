2f8956497c37b849ba4c42b228d828decfe5dfc0
IE-MangChi <hsm983278@gmail.com>
2023-11-14 21:23:49 +0900

    feat: 음료만 주문시 에러를 발생시키는 기능 추가

--- src/main/java/christmas/domain/order/OrderAmount.java
+++ src/main/java/christmas/domain/order/OrderAmount.java
@@ -1,8 +1,12 @@
 package christmas.domain.order;
 
 import christmas.domain.menu.MenuCategory;
+import christmas.domain.order.dto.OrderDto;
+import christmas.exception.ChristmasException;
+import christmas.exception.ErrorMessage;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 
 public class OrderAmount {
 
@@ -13,7 +17,7 @@ public class OrderAmount {
     }
 
     public static OrderAmount of(Order order) {
-        Map<MenuCategory, Integer> orderData = order.getOrder().order();
+        Map<MenuCategory, Integer> orderData = validateOnlyBeverage(order.getOrder());
         long sum = 0;
         for (Entry<MenuCategory, Integer> entry : orderData.entrySet()) {
             sum += entry.getKey().getPrice() * entry.getValue();
@@ -23,5 +27,21 @@ public class OrderAmount {
 
     public long getTotalAmount() {
         return this.totalAmount;
+    }
+
+    private static Map<MenuCategory, Integer> validateOnlyBeverage(OrderDto orderDto) {
+        Map<MenuCategory, Integer> order = orderDto.order();
+        Optional<MenuCategory> findNotBeverage = order.keySet().stream()
+                .filter(menu -> !isBeverage(menu))
+                .findFirst();
+        if (findNotBeverage.isEmpty()) {
+            throw ChristmasException.of(ErrorMessage.INVALID_ORDER);
+        }
+        return order;
+    }
+
+    private static boolean isBeverage(MenuCategory menu) {
+        return menu.equals(MenuCategory.CHAMPAGNE) | menu.equals(MenuCategory.ZERO_COLA)
+                | menu.equals(MenuCategory.RED_WINE);
     }
 }
