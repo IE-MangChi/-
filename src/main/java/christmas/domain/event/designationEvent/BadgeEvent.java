a08e3a75a1cedff52894776241cc15818d6efc94
IE-MangChi <hsm983278@gmail.com>
2023-11-14 14:17:02 +0900

    feat: 이벤트 뱃지 기능 추가
    - getBadgeByTotalBenefitAmount: 총혜택금액으로 뱃지를 반환하는 기능

---
+++ src/main/java/christmas/domain/event/designationEvent/BadgeEvent.java
@@ -0,0 +1,27 @@
+package christmas.domain.event.designationEvent;
+
+public enum BadgeEvent {
+
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타");
+
+    private final String badge;
+
+    BadgeEvent(String badge) {
+        this.badge = badge;
+    }
+
+    public BadgeEvent getBadgeByTotalBenefitAmount(long amount) {
+        if (amount >= EventConfig.STAR_BADGE_MINIMUM_AMOUNT) {
+            return STAR;
+        }
+        if (amount >= EventConfig.TREE_BADGE_MINIMUM_AMOUNT) {
+            return TREE;
+        }
+        if (amount >= EventConfig.SANTA_BADGE_MINIMUM_AMOUNT) {
+            return SANTA;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/christmas/domain/event/designationEvent/EventConfig.java
@@ -0,0 +1,9 @@
+package christmas.domain.event.designationEvent;
+
+public class EventConfig {
+
+    // 이벤트 배지
+    protected static final long STAR_BADGE_MINIMUM_AMOUNT = 5_000;
+    protected static final long TREE_BADGE_MINIMUM_AMOUNT = 10_000;
+    protected static final long SANTA_BADGE_MINIMUM_AMOUNT = 20_000;
+}
