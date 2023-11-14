647ea154533eb253d40389b23f7d5a1fbcf23e60
IE-MangChi <hsm983278@gmail.com>
2023-11-14 19:56:01 +0900

    refactor: 에러메시지 분리

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -2,6 +2,8 @@ package christmas.domain.order;
 
 import christmas.domain.menu.MenuCategory;
 import christmas.domain.order.dto.OrderDto;
+import christmas.exception.ChristmasException;
+import christmas.exception.ErrorMessage;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,22 +14,14 @@ public class Order {
     private final Map<MenuCategory, Integer> orderItem = new HashMap<>();
     private long totalQuantity = 0L;
 
-    public Order() {}
-
-    private void put(final MenuCategory menuCategory, final Integer count) {
-        orderItem.put(menuCategory, count);
+    public Order() {
     }
 
     public void plus(final String menu, final Integer count) {
-        if (canOrder(count)) {
-            MenuCategory dish = validateMenu(menu);
-            put(dish, count);
-            totalQuantity += count;
-            return;
-        }
-        throw new IllegalArgumentException(
-                "[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.\n"
-                        + "(e.g. 시저샐러드-1, 티본스테이크-1, 크리스마스파스타-1, 제로콜라-3, 아이스크림-1의 총개수는 7개)");
+        validateOrderCount(count);
+        MenuCategory dish = validateIsInMenu(menu);
+        put(dish, count);
+        totalQuantity += count;
     }
 
     public int getMenuByMenuNames(List<MenuCategory> menuNames) {
@@ -41,21 +35,25 @@ public class Order {
         return sum;
     }
 
-    private static MenuCategory validateMenu(String menu) {
+    public OrderDto getOrder() {
+        return new OrderDto(orderItem);
+    }
+
+    private static MenuCategory validateIsInMenu(String menu) {
         Optional<MenuCategory> dish = MenuCategory.getDishDataByName(menu);
         if (dish.isEmpty()) {
-            throw new IllegalArgumentException(
-                    "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
-            );
+            throw ChristmasException.of(ErrorMessage.INVALID_ORDER);
         }
         return dish.get();
     }
 
-    private boolean canOrder(Integer count) {
-        return totalQuantity + count <= OrderConfig.MAXIMUM_ORDER_AMOUNT;
+    private void put(final MenuCategory menuCategory, final Integer count) {
+        orderItem.put(menuCategory, count);
     }
 
-    public OrderDto getOrder() {
-        return new OrderDto(orderItem);
+    private void validateOrderCount(Integer count) {
+        if (count >= OrderConfig.MAXIMUM_ORDER_AMOUNT) {
+            throw ChristmasException.of(ErrorMessage.MAXIMUM_MENU_QUANTITY_PER_ORDER);
+        }
     }
 }
