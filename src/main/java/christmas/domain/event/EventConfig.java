2102321be28029cc070fb32ae2ca818541f5a2f8
IE-MangChi <hsm983278@gmail.com>
2023-11-14 13:43:27 +0900

    feat: 평일 할인 기능 추가
    - support: 할인이 가능한지 판단하는 기능
    - discount: 할인 적용 기능

--- src/main/java/christmas/domain/event/EventConfig.java
+++ src/main/java/christmas/domain/event/EventConfig.java
@@ -1,7 +1,12 @@
 package christmas.domain.event;
 
+import christmas.domain.menu.MenuCategory;
+import java.util.List;
+
 public class EventConfig {
 
+    public static final int YEAR = 2023;
+    public static final int MONTH = 12;
     public static final int INITAL_DISCOUNT_AMOUNT = 1_000;
     public static final int DAILY_DISCOUNT_INCREASE = 100;
     public static final int START_DATE = 1;
@@ -9,4 +14,7 @@ public class EventConfig {
     public static final int MINIMUM_ORDER_AMOUNT_FOR_GIFT_EVENT = 120_000;
     public static final int[] SPECIAL_DISCOUNT_DAY = {3, 10, 17, 24, 25, 31};
     public static final Integer SPECIAL_DISCOUNT_AMOUNT = 1_000;
+    public static final List<String> WEEKDAY_DISCOUNT_DAY_OF_WEEK = List.of("SUNDAY", "MONDAY", "TUESDAY", "THURSDAY");
+    public static final List<MenuCategory> WEEKDAY_DISCOUNT_MENU = List.of(MenuCategory.CHOCO_CAKE, MenuCategory.ICE_CREAM);
+    public static final long WEEKDAY_DISCOUNT_AMOUNT = 2_023;
 }
---
+++ src/main/java/christmas/domain/event/WeekdayEvent.java
@@ -0,0 +1,30 @@
+package christmas.domain.event;
+
+import christmas.domain.order.Order;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+public enum WeekdayEvent implements Event<Integer, Long, Order>{
+
+    WEEKDAY_EVENT(EventConfig.WEEKDAY_DISCOUNT_DAY_OF_WEEK);
+
+    private List<String> discountWeekOfDay;
+
+    WeekdayEvent(List<String> discountWeekOfDay) {
+        this.discountWeekOfDay = discountWeekOfDay;
+    }
+
+    @Override
+    public boolean support(Integer date) {
+        LocalDate weekOfDay = LocalDate.of(EventConfig.YEAR, EventConfig.MONTH, date);
+        return discountWeekOfDay.stream()
+                .anyMatch(day -> day.equals(weekOfDay));
+    }
+
+    @Override
+    public Long discount(Order order) {
+        int discountMenuCount = order.getMenuByMenuNames(EventConfig.WEEKDAY_DISCOUNT_MENU);
+        return discountMenuCount * EventConfig.WEEKDAY_DISCOUNT_AMOUNT;
+    }
+}
