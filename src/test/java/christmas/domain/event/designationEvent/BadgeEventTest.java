e2239e549dd2d278c117b5744384294b1c686b80
IE-MangChi <hsm983278@gmail.com>
2023-11-14 18:28:21 +0900

    test: 배지 이벤트 테스트 코드 추가

---
+++ src/test/java/christmas/domain/event/designationEvent/BadgeEventTest.java
@@ -0,0 +1,23 @@
+package christmas.domain.event.designationEvent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BadgeEventTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0:NOTHING", "2000:NOTHING", "4999:NOTHING",
+            "5000:STAR", "7000:STAR", "9999:STAR",
+            "10000:TREE", "15000:TREE", "19999:TREE",
+            "20000:SANTA", "22000:SANTA", "30000:SANTA"}, delimiter = ':')
+    @DisplayName("금액에 따른 이벤트 뱃지를 지급해야한다.")
+    void BadgeEventTest(long amount, BadgeEvent expected) {
+        Assertions.assertThat(BadgeEvent.getBadgeByTotalBenefitAmount(amount)).isEqualTo(expected);
+    }
+
+}
